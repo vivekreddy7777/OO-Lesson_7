57310c764ae38dbb91ce721f33e723713ca4d68e
vivekreddy <vivekreddy7777@gmail.com>
2020-11-14 01:34:23 -0600

    7

--- view/MyCanvas.java
+++ view/MyCanvas.java
@@ -4,13 +4,19 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 import javax.swing.JPanel;
+
+import model.GameElement;
+
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.util.ArrayList;
 
 
 public class MyCanvas extends JPanel {
 
     private GameBoard gameBoard;
+    private ArrayList<GameElement> gameElements =new ArrayList<>();
+
     public MyCanvas(GameBoard gameBoard,int width,int height){
         this.gameBoard=gameBoard;
         setBackground(Color.black);
@@ -22,6 +28,10 @@ public class MyCanvas extends JPanel {
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         Graphics2D g2= (Graphics2D) g;
+
+        for (var e:gameElements){
+            e.render(g2);
+        }
 
     }
     
